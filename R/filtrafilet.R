5b405dd62a19b005e1ff63eb1f458e4ef5b53c08
Kleber Infante <corujito@gmail.com>
2016-03-26 09:24:24 -0300

    tentando gerar grafico

--- R/filtrafilet.R
+++ R/filtrafilet.R
@@ -3,8 +3,6 @@ filtrafilet <- function(mydata){
 }
 
 geragrafico <- function(mydata, nameColumnToPlot, ...){
-  # barplot(table(mydata$nameColumnToPlot)[1:10,], type="l", col="blue", xlab = "Ano de publicação", 
-  #         ylab = "Quantidade de artigos", main = "Quantidade de artigos publicados por ano")
-	hist(as.numeric(mydata$nameColumnToPlot), nclass = 4, xlab = "JCR", ylab = "Quantidade de artigos", 
-          main = "Distribuição de artigos por JCR", col="blue")
+  plot(table(mydata$Publication.Year), type="l", col="blue", xlab = "Ano de publicação", 
+          ylab = "Quantidade de artigos", main = "Quantidade de artigos publicados por ano")
 }
