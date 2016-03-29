4593af49810497a2786d058e63889292a49e7740
Kleber Infante <corujito@gmail.com>
2016-03-29 18:59:26 -0300

    tentando graficar filtrosush

--- R/filtrafilet.R
+++ R/filtrafilet.R
@@ -6,7 +6,7 @@ filtrafilet <- function(dados, jcrmin, anomin, citano, porcpareto) {
     ## Base com JCR ##
     #jcr <- read.csv('data/JournalHomeGrid.csv', header = F, sep = ",", stringsAsFactors = FALSE)
     #jcr <- jcr[-1,]
-    jcr[["Full Journal Title"]] <- toupper(jcr[["Full Journal Title"]]) #Deixando o título dos Journals todo com letra maiuscula, pois será utilizado como chave
+    #jcr[["Full Journal Title"]] <- toupper(jcr[["Full Journal Title"]]) #Deixando o título dos Journals todo com letra maiuscula, pois será utilizado como chave
     
     dados.jcr <- merge(x=dados, y=jcr, by.x = "Source.Title", by.y = "Full Journal Title", all.x = TRUE)
     
