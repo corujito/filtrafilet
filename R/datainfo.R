20976a809696500ac4642d2169ae4b4c67ef3e8a
Kleber Infante <corujito@gmail.com>
2016-05-14 20:53:06 -0300

    correcao as.list

--- R/datainfo.R
+++ R/datainfo.R
@@ -1,7 +1,7 @@
 datainfo <- function(mydata){
   list(
     #all = as.list(colnames(mydata)),
-    all = as.list("Publication.Year", "Authors", "Source.Title", "V5"),
+    all = as.list(c("Publication.Year", "Authors", "Source.Title", "V5")),
     strings = as.list(names(which(unlist(lapply(mydata, is.character))))),
     other = as.list(names(which(unlist(lapply(mydata, function(x){!is.character(x)})))))
   )
